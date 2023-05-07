d316bfafa92dfe36ac6f913c906ef61cc8372f09
Luis Santos <luishv551@gmail.com>
2023-05-07 19:53:23 -0300

    Estudando Ponteiros 07/05/2023

---
+++ Exercises/Ponteiros_1.c
@@ -0,0 +1,17 @@
+int main()
+{
+
+int x=5;
+int *y;
+y=&x; //recebendo o endereco de memoria da variavel x como conteudo
+
+printf("Conteudo de x: %d\n", x);
+printf("Endereco de x: %d\n", &x);
+
+printf("Conteudo de y: %d\n", y);
+printf("Conteudo da variavel apontada por y: %d\n", *y);
+
+
+*y = 10;
+printf("Conteudo de x: %d", x);
+}
---
+++ Exercises/Ponteiros_2.c
@@ -0,0 +1,10 @@
+int main()
+{
+
+int *x;
+int y = 5;
+
+x = &y;
+
+
+}
